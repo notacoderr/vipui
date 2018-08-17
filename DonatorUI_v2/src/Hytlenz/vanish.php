fde0ac4c656c194dff88de09ee9f5c38e9177472
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-17 16:48:20 +0800

    Create vanish.php
    for vanishUi

---
+++ DonatorUI_v2/src/Hytlenz/vanish.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Hytlenz;
+
+use pocketmine\entity\{Effect, EffectInstance};
+use pocketmine\Player;
+use Hytlenz\DonatorUI;
+
+class vanish {
+
+public $main;
+	
+public function __construct(DonatorUI $pg)
+{
+  $this->main = $pg;
+}
+
+public function vanishUI($sender)
+{
+      $api = $this->main->getServer()->getPluginManager()->getPlugin("FormAPI");
+      $form = $api->createSimpleForm(function (Player $sender, $data){
+            $result = $data;
+            if ($result == null) {
+            }
+            switch ($result) {
+                    case 0:
+                      break;
+                    case 1:
+                      $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), 99999999, 0, false));
+                      $sender->addTitle("§bVanish", "§aEnabled!");
+                      break;
+                    case 2:
+                      $sender->removeEffect(Effect::INVISIBILITY);
+                      $sender->addTitle("§bVanish", "§cDisabled!");
+                      break;
+                    case 3:
+                      $this->main->getServer()->getCommandMap()->dispatch($sender, "vip");
+                      break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("vanish.title"));
+        $form->setContent($this->getConfig()->get("vanish.content"));
+        $form->addButton("§lExit");
+        $form->addButton("§l§2On");
+        $form->addButton("§l§4Off");
+        $form->addButton("§lBack");
+        $form->sendToPlayer($sender);
+}
