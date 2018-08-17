0c6e7e89b53037509be2383abe80af7ff2ed4247
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-17 16:59:00 +0800

    Update DonatorUI.php

--- DonatorUI_v2/src/Hytlenz/DonatorUI.php
+++ DonatorUI_v2/src/Hytlenz/DonatorUI.php
@@ -2,130 +2,97 @@
 
 namespace Hytlenz;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
-use pocketmine\entity\Effect;
-use pocketmine\entity\EffectInstance;
 use jojoe77777\FormAPI;
-use pocketmine\Player;
-use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
 
-class DonatorUI extends PluginBase implements Listener{
+use pocketmine\{Player,
+		Server};
+
+use pocketmine\command\{Command,
+			CommandSender};
+
+class DonatorUI extends PluginBase implements Listener {
     
-    public function onEnable() : void
-    {
-        $this->getLogger()->info("[HytFormUI] - DonatorUI Enabled!");
-        Server::getInstance()->getPluginManager()->registerEvents($this, $this);
-        $this->checkDepends();
-	    
-        $this->api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-	    
-        @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
-        $this->getResource("config.yml");
+    	public function onEnable() : void
+    	{
+		$this->getLogger()->info("[HytFormUI] - DonatorUI - VIP UI Enabled!");
+		Server::getInstance()->getPluginManager()->registerEvents($this, $this);
 
-    }
+		$this->checkDepends();
 
-    public function checkDepends() : void
-    {
-        if(is_null(Server::getInstance()->getPluginManager()->getPlugin("FormAPI"))){
-            $this->getLogger()->info("§4Please install FormAPI Plugin, disabling plugin...");
-            $this->getPluginLoader()->disablePlugin($this);
-        }
-    }
+		$this->api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
 
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool
-    {
-        switch($cmd->getName()){
-        case "vip":
-        if(!($sender instanceof Player))
-	{
-        	$sender->sendMessage("§7This command must be used ingame!");
-                return true;
+		$this->vanishUI = new vanish($this);
+
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+		$this->getResource("config.yml");
+
     	}
-			
-        
-			
-        $form = $api->createSimpleForm(function (Player $sender, $data){
-            $result = $data;
-            if ($result == null) {
-            }
-            switch ($result) {
-                    case 0:
-                        break;
-                    case 1:
-			    $sender->setHealth(20);
-			    $sender->setFood(20);
-			    $sender->sendMessage($this->getConfig()->get("cure.msg"));
-				break;
-                    case 2:
-                    $this->flyUI($sender);
-                        break;
-                    case 3:
-                    $this->vanishUI($sender);
-                        break;
-                    case 4:
-                    $this->lightsUI($sender);
-                        break;
-                    case 5:
-                    $this->gmUI($sender);
-                        break;
-                    case 6:
-                    $this->nickUI($sender);
-                        break;
 
-            }
-        });
-        $form->setTitle($this->getConfig()->get("donator.title"));
-        $form->setContent($this->getConfig()->get("donator.content"));
-        $form->addButton("§4Exit");
-        $form->addButton("§lCure");
-        $form->addButton("§lFly");
-        $form->addButton("§lVanish");
-        $form->addButton("§lLights");
-        $form->addButton("§lGamemode");
-        $form->addButton("§lNickname");
-        $form->sendToPlayer($sender);       
-        }
-        return true;
-    }
-    
-     public function vanishUI($sender){
-      $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-      $form = $api->createSimpleForm(function (Player $sender, $data){
-            $result = $data;
-            if ($result == null) {
-            }
-            switch ($result) {
-                    case 0:
-                    $sender->addTitle("§bCancelled", "§aYour Request");
-                        break;
-                    case 1:
-                    $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), 99999999, 0, false));
-                    $sender->addTitle("§bVanish", "§aEnabled!");
-                    $sender->sendMessage($this->getConfig()->get("vanish.on"));
-                        break;
-                    case 2:
-                    $sender->removeEffect(Effect::INVISIBILITY);
-                    $sender->addTitle("§bVanish", "§cDisabled!");
-                    $sender->sendMessage($this->getConfig()->get("vanish.off"));
-                        break;
-                    case 3:
-                    $command = "donator" ;
-                    $this->getServer()->getCommandMap()->dispatch($sender, $command);
-                        break;
-            }
-        });
-        $form->setTitle($this->getConfig()->get("vanish.title"));
-        $form->setContent($this->getConfig()->get("vanish.content"));
-        $form->addButton("§lExit");
-        $form->addButton("§l§2On");
-        $form->addButton("§l§4Off");
-        $form->addButton("§lBack");
-        $form->sendToPlayer($sender);
-        }
+    	public function checkDepends() : void
+    	{
+        	if(is_null(Server::getInstance()->getPluginManager()->getPlugin("FormAPI")))
+		{
+			$this->getLogger()->info("§4Please install FormAPI Plugin, disabling plugin...");
+			$this->getPluginLoader()->disablePlugin($this);
+        	}
+	}
+
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool
+	{
+        	if(strtolower($cmd->getName()) === "vip")
+		{
+			if(!($sender instanceof Player))
+			{
+        			$sender->sendMessage("§7This command must be used ingame!");
+                		return true;
+    			}
+			
+        		$form = $api->createSimpleForm(function (Player $sender, $data){
+            		$result = $data;
+            			if ($result == null) {
+					return;
+            			}
+            			switch ($result) {
+                    		case 0:
+                        		break;
+                    		case 1:
+			    		$sender->setHealth(20);
+			    		$sender->setFood(20);
+			    		$sender->sendMessage($this->getConfig()->get("cure.msg"));
+					break;
+                    		case 2:
+                    			$this->flyUI($sender);
+                        		break;
+                    		case 3:
+                    			$this->vanishUI($sender);
+                        		break;
+                    		case 4:
+                    			$this->lightsUI($sender);
+                        		break;
+                   		case 5:
+                    			$this->gmUI($sender);
+                        		break;
+                    		case 6:
+                   			$this->nickUI($sender);
+                        		break;
+            			}
+        		});
+			$form->setTitle($this->getConfig()->get("donator.title"));
+			$form->setContent($this->getConfig()->get("donator.content"));
+			$form->addButton("§4Exit");
+			$form->addButton("§lCure");
+			$form->addButton("§lFly");
+			$form->addButton("§lVanish");
+			$form->addButton("§lLights");
+			$form->addButton("§lGamemode");
+			$form->addButton("§lNickname");
+			$form->sendToPlayer($sender);       
+        	}
+        	return true;
+	}
         
       public function lightsUI($sender){
       $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
@@ -244,7 +211,8 @@ class DonatorUI extends PluginBase implements Listener{
 				$form->sendToPlayer($sender);
 		}
 
-    public function onDisable(){
-        $this->getLogger()->info("[HytFormUI] - DonatorUI Disabled!");
+    public function onDisable() : void
+    {
+        $this->getLogger()->info("[HytFormUI] - DonatorUI - VIPUI Disabled!");
     }
 }
