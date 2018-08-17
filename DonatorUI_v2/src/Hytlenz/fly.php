9e75e55d624be6fa3e91eddbcbe6093316a89eab
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-17 17:05:42 +0800

    Create fly.php

---
+++ DonatorUI_v2/src/Hytlenz/fly.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hytlenz;
+
+use pocketmine\entity\{Effect, EffectInstance};
+use pocketmine\Player;
+use Hytlenz\DonatorUI;
+
+class fly {
+
+	public $main;
+
+	public function __construct(DonatorUI $pg)
+	{
+	  $this->main = $pg;
+	}
+
+  public function sendUI($sender){
+      $api = $this->main->getServer()->getPluginManager()->getPlugin("FormAPI");
+      $form = $api->createSimpleForm(function (Player $sender, $data){
+            $result = $data;
+            if ($result == null) {
+              return;
+            }
+            switch ($result) {
+                    case 0:
+                       break;
+                    case 1:
+                      $sender->setAllowFlight(true);
+                      $sender->addTitle("§bFlying", "§aEnabled!");
+                       break;
+                    case 2:
+                      $sender->setAllowFlight(false);
+                      $sender->addTitle("§bFlying", "§cDisabled!");
+                       break;
+                    case 3:
+                      $this->main->getServer()->getCommandMap()->dispatch($sender, "vip");
+                       break;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("fly.title"));
+        $form->setContent($this->getConfig()->get("fly.content"));
+        $form->addButton("§lExit");
+        $form->addButton("§l§2On");
+        $form->addButton("§l§4Off");
+        $form->addButton("§lBack");
+        $form->sendToPlayer($sender);
+  }
+}
