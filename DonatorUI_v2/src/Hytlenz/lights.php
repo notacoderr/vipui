bc7c4f272f9b6fc961df53110fa83f76b800d80d
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-19 20:03:57 +0800

    Create lights.php

---
+++ DonatorUI_v2/src/Hytlenz/lights.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Hytlenz;
+
+use pocketmine\entity\{Effect,
+                      EffectInstance};
+use pocketmine\Player;
+use Hytlenz\DonatorUI;
+
+class lights {
+
+	public $main;
+
+	public function __construct(DonatorUI $pg)
+	{
+	  $this->main = $pg;
+	}
+
+public function sendUI($sender) {
+
+      $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+      $form = $api->createSimpleForm(function (Player $sender, $data) {
+      
+            $result = $data;
+            
+            if ($result == null) {
+              return;
+            }
+            
+            switch ($result) {
+                    case 0:
+                        break;
+                        
+                    case 1:
+                      $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 99999999, 0, false));
+                      $sender->addTitle("§bLights on", "§0[§e-=-=-=-=-=-=-=-§0]");
+                      //$sender->sendMessage($this->getConfig()->get("lights.on"));
+                        break;
+                        
+                    case 2:
+                      $sender->removeEffect(Effect::NIGHT_VISION);
+                      $sender->addTitle("§cLights off", "§0[§7-=-=-=-=-=-=-=-§0]");
+                        break;
+                        
+                    case 3:
+                      $command = "vip";
+                      $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+            }
+        });
+        
+          $form->setTitle($this->getConfig()->get("lights.title"));
+          $form->setContent($this->getConfig()->get("lights.content"));
+          $form->addButton("§lExit");
+          $form->addButton("§l§2On");
+          $form->addButton("§l§4Off");
+          $form->addButton("§lBack");
+          $form->sendToPlayer($sender);
+ 
+        }
+}
